4fa5986a30f3048dad66a6b8db82c7daf7060a0b
josscimat <37633766+josscimat@users.noreply.github.com>
2021-06-11 13:53:01 -0500

    Add files via upload

---
+++ GeMMS-Round3/Reference_Implementation/RedGeMSS256/include/gcd_gf2nx.h
@@ -0,0 +1,18 @@
+#ifndef _GCD_GF2NX_H
+#define _GCD_GF2NX_H
+
+#include "prefix_name.h"
+#include "arch.h"
+#include "gf2nx.h"
+
+
+unsigned int PREFIX_NAME(gcd_gf2nx)(unsigned int* b, gf2nx A, unsigned int da,
+                                                     gf2nx B, unsigned int db);
+unsigned int PREFIX_NAME(gcdES_gf2nx)(unsigned int *b,gf2nx A,unsigned int da,
+                                                      gf2nx B,unsigned int db);
+#define gcd_gf2nx PREFIX_NAME(gcd_gf2nx)
+#define gcdES_gf2nx PREFIX_NAME(gcdES_gf2nx)
+
+
+#endif
+
